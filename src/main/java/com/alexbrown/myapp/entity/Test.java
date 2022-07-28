0e2f2d1dc1cd02ad532fb8cb45ea642c5e7112f2
Jesús Alejandro Marrón Legorreta <j_marron@NGK_NTK.COM.MX>
2022-07-28 12:25:30 -0500

    Nombre del proyecto: my-app, Descripcion: Version final del proyecto my-app

---
+++ src/main/java/com/alexbrown/myapp/entity/Test.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.alexbrown.myapp.entity;
+
+/**
+ * @author Jesus Alejandro Marron Legorreta
+ *
+ */
+public class Test {
+
+	/**
+	 * Propiedad de prueba para la muestra de complilacion del archivo testentity
+	 */
+	public String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+}
